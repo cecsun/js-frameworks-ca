5f4fee959e72d443efe65290a996a478756ff3c7
cecsun <ceciliesunde94@gmail.com>
2024-05-23 17:28:19 +0200

    Created header with a layout component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <div>
+        <h2>Welcome to My App</h2>
+        <p>This is the main content area.</p>
+      </div>
+    </Layout>
   );
 }
 
