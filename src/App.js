44631c0748c50bdfe805372b00ab22429016c870
cecsun <ceciliesunde94@gmail.com>
2024-05-29 15:23:32 +0200

    Added the different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Layout } from './components/Layout/Layout';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './Pages/Home/Home';
+import { Contact } from './Pages/Contact/Contact';
+import { Products } from './Pages/Products/Products';
+import { RouteNotFound } from './Pages/RouteNotFound/RouteNotFound';
 
 function App() {
   return (
-    <Layout />
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,6 @@ import * as S from "./Nav.styles";
 
 function Nav() {
   return (
-    <BrowserRouter>
       <S.Navbar>
         <S.NavList>
             <S.NavItem>
@@ -13,18 +12,12 @@ function Nav() {
             <S.NavItem>
                 <S.StyledLink to="/contact">Contact</S.StyledLink>
             </S.NavItem>
-            <S.NavItem>
+            {/* <S.NavItem>
                 <S.StyledLink to="/cart">Cart</S.StyledLink>
-            </S.NavItem>
+            </S.NavItem> */}
         </S.NavList>
       </S.Navbar>
-      <Routes>
-        <Route index element={<div></div>} />
-        <Route path="contact" element={<div></div>} />
-        <Route path="cart" element={<div></div>} />
-        <Route path="*" element={<div>Route not found</div>} />
-      </Routes>
-    </BrowserRouter>
+   
   )
 }
 
