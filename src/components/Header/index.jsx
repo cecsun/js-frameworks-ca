22ecc3a45779c75cba75176227225d9158f61fdd
cecsun <ceciliesunde94@gmail.com>
2024-06-03 11:43:23 +0200

    Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from './index.styles';
+import Nav from '../Nav/index.jsx';
+
+export function Header() {
+  return (
+    <S.Header>
+      <Nav />
+    </S.Header>
+  );
+}
