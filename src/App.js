baf82050361873746f4fb912c90f4bf719dbf5b7
cecsun <ceciliesunde94@gmail.com>
2024-06-03 14:43:57 +0200

    Moved CSS import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
 import Layout from './components/Layout/index.jsx';
 import HomePage from './Pages/HomePage/index.jsx';
 import SingleProductPage from './Pages/SingleProductPage/index.jsx';
