407e8984afeb52121f24eebccecaac309c914649
cecsun <ceciliesunde94@gmail.com>
2024-06-03 11:41:38 +0200

    API

---
+++ src/common/constants.js
@@ -0,0 +1,4 @@
+const API_BASE_URL = 'https://v2.api.noroff.dev';
+
+export const API_PRODUCTS_URL = `${API_BASE_URL}/online-shop`;
+export const API_SINGLE_PRODUCT_URL = `${API_BASE_URL}/online-shop`;
