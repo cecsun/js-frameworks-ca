ea6b8ed79ad7391fbf968cc043267f92a6852764
cecsun <ceciliesunde94@gmail.com>
2024-06-03 11:41:09 +0200

    Moved BrowserRouter to App.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
-// ...imports above
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
