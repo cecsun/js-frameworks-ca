c6b02b09db79c1622c1a40e7f6f121bf0c979637
cecsun <ceciliesunde94@gmail.com>
2024-06-03 11:50:30 +0200

    Created ProductsPage

---
+++ src/Pages/ProductsPage/index.jsx
@@ -0,0 +1,11 @@
+import ProductsList from '../../components/ProductsList';
+
+function ProductsPage() {
+  return (
+    <div>
+      <ProductsList />
+    </div>
+  );
+}
+
+export default ProductsPage;
