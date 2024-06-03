7a6b5435552117cf059e0e507f8ded6a5216224e
cecsun <ceciliesunde94@gmail.com>
2024-06-03 11:42:43 +0200

    Created Cart Icon

---
+++ src/components/CartIcon/index.jsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
+
+const SIZE = '15';
+
+function CartIcon() {
+  const { cart, addToCart } = useContext(CartContext);
+  return (
+    <div>
+      <div
+        style={{
+          position: 'relative',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        🛒
+        <div
+          style={{
+            background: 'red',
+            borderRadius: '50%',
+            width: `${SIZE}px`,
+            height: `${SIZE}px`,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+          }}
+        >
+          {cart.length.toString()}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartIcon;
