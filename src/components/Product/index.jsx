572113d824f616c55a8436f41f9093a36ee868d3
cecsun <ceciliesunde94@gmail.com>
2024-06-04 20:22:24 +0200

    Add product cards from cart to CheckoutPage

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -4,10 +4,12 @@ import * as S from './index.styles.js';
 export default function Product({
   productDetails,
   handleAddToCartButtonClick,
+  handleRemoveFromCartButtonClick,
+  checkout = false,
 }) {
   const { description, discountedPrice, image, price, title, id } = productDetails;
   function handleButtonClick() {
-    handleAddToCartButtonClick(productDetails);
+    checkout ? handleRemoveFromCartButtonClick(productDetails) : handleAddToCartButtonClick(productDetails);
   }
 
   return (
@@ -15,10 +17,12 @@ export default function Product({
       <h3>{title}</h3>
       <img src={image.url} alt={title} style={{ width: '100%', height: '100%' }} />
       <p>{description}</p>
-      <S.DiscountedPrice>{discountedPrice}</S.DiscountedPrice>
+      {discountedPrice < price && <S.DiscountedPrice>{discountedPrice}</S.DiscountedPrice>}
       <S.Price>{price}</S.Price>
-      <S.StyledLink to={`/product/${id}`}>View</S.StyledLink>
-      <S.Button onClick={handleButtonClick}>Add to cart</S.Button>
+      <S.StyledLink to={`/product/${id}`}>
+        <button>View Product</button>
+      </S.StyledLink>
+      <S.Button onClick={handleButtonClick}>{checkout ? "Remove from cart" : "Add to cart"}</S.Button>
     </S.ProductsContainer>
   );
 }
