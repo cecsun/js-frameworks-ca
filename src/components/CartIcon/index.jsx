0ae6ee5baf94a3f2581b8e7a3917e3e303b19273
cecsun <ceciliesunde94@gmail.com>
2024-06-04 20:20:59 +0200

    Navigate cart to CheckoutPage

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -2,12 +2,19 @@ import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
 import * as S from './index.styles.js';
 import { FaShoppingCart } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 function CartIcon() {
   const { cart } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const handleCartClick = () => {
+    navigate('/checkoutPage');
+  };
+
   return (
     <div>
-      <S.CartWrapper>
+      <S.CartWrapper onClick={handleCartClick}>
         <S.CartIcon>
           <FaShoppingCart />
         </S.CartIcon>
