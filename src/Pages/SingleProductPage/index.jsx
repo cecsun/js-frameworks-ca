5180f5ed22c26b82df895fb777ccae771a7f629a
cecsun <ceciliesunde94@gmail.com>
2024-06-04 20:24:57 +0200

    Added reviews to SingleProductPage

--- src/Pages/SingleProductPage/index.jsx
+++ src/Pages/SingleProductPage/index.jsx
@@ -25,15 +25,22 @@ function SingleProductPage() {
   if (hasError) {
     return <div>Error loading data</div>;
   }
-
+  console.log(data)
   if (data) {
     return (
       <div>
         <ul>
           <li>Title: {data?.data?.title} </li>
           <li>Description: {data?.data?.description} </li>
-          <li>Discounted Price: {data?.data?.discountedPrice}</li>
+          {data?.data?.discountedPrice < data?.data?.price && <li>Discounted Price: {data?.data?.discountedPrice}</li>}
           <li>Price: {data?.data?.price}</li>
+          {data?.data?.reviews.map((review) => (
+            <div key={review.id}>
+              <li>Review: {review.username}</li>
+              <li>Rating: {review.rating}</li>
+              <li>Comment: {review.description}</li>
+            </div>
+          ))}
         </ul>
         {data?.data?.image.url && <img src={data?.data?.image.url} alt={data?.data?.title} />}
         <button onClick={handleButtonClick}>Add to cart</button>
