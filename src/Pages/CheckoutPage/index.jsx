48e3515d7d4cb6d04a937108394e0f3c97673475
cecsun <ceciliesunde94@gmail.com>
2024-06-04 20:24:32 +0200

    CheckoutPage

--- src/Pages/CheckoutPage/index.jsx
+++ src/Pages/CheckoutPage/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
+import Product from '../../components/Product';
 
 function CheckoutPage() {
-    return <div>CheckoutPage</div>
+    const { cart, removeFromCart } = useContext(CartContext);
+    function handleRemoveFromCartButtonClick(productDetails) {
+      removeFromCart(productDetails.id);
+    }
+    return (
+      <div>
+        <div>CheckoutPage</div>
+        {cart.map((productDetails) => (
+          <Product
+            key={productDetails.id}
+            productDetails={productDetails}
+            checkout={true}
+            handleRemoveFromCartButtonClick={handleRemoveFromCartButtonClick}
+            // handleAddToCartButtonClick={handleAddToCartButtonClick}
+          />
+        ))}
+        <div>total: {Math.round(cart.reduce((a, {discountedPrice}) => a + discountedPrice, 0)*100)/100}</div>
+        <div>checkout</div>
+      </div>
+
+    );
   }
 
 export default CheckoutPage;
