6739f5593bb69123b7192f0ad9c424032864d8d7
cecsun <ceciliesunde94@gmail.com>
2024-06-05 15:18:29 +0200

    Content

--- src/Pages/CheckoutSuccessPage/index.jsx
+++ src/Pages/CheckoutSuccessPage/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import * as S from "./index.styles";
 
 function CheckoutSuccessPage() {
-    return <div>CheckoutSuccessPage</div>
+    return (
+      <S.CheckoutSuccessPageContainer>
+        <h1>Checkout Successful</h1>
+        <p>Thank you for your purchase!</p>
+        <S.StyledLink to="/">Go back to the store</S.StyledLink>
+      </S.CheckoutSuccessPageContainer>
+    )
   }
 
 export default CheckoutSuccessPage;
