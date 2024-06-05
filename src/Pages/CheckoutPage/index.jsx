02a4bec3328b13dfc94e8626defce38d7817c124
cecsun <ceciliesunde94@gmail.com>
2024-06-05 15:17:50 +0200

    Checkout styling and button

--- src/Pages/CheckoutPage/index.jsx
+++ src/Pages/CheckoutPage/index.jsx
@@ -2,27 +2,32 @@ import React from "react";
 import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
 import Product from '../../components/Product';
+import * as S from "./index.styles";
 
 function CheckoutPage() {
     const { cart, removeFromCart } = useContext(CartContext);
     function handleRemoveFromCartButtonClick(productDetails) {
       removeFromCart(productDetails.id);
     }
+
+    function handleCheckoutButtonClick() {
+      window.location.href = '/checkoutSuccessPage';
+    }
+
     return (
-      <div>
-        <div>CheckoutPage</div>
+      <S.CheckoutContainer>
+        <h1>Checkout</h1>
         {cart.map((productDetails) => (
           <Product
             key={productDetails.id}
             productDetails={productDetails}
             checkout={true}
             handleRemoveFromCartButtonClick={handleRemoveFromCartButtonClick}
-            // handleAddToCartButtonClick={handleAddToCartButtonClick}
           />
         ))}
-        <div>total: {Math.round(cart.reduce((a, {discountedPrice}) => a + discountedPrice, 0)*100)/100}</div>
-        <div>checkout</div>
-      </div>
+        <S.Total>Total: {Math.round(cart.reduce((a, {discountedPrice}) => a + discountedPrice, 0)*100)/100}</S.Total>
+        <S.Button onClick={handleCheckoutButtonClick}>Checkout</S.Button>
+      </S.CheckoutContainer>
 
     );
   }
