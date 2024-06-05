a9e86364e01441b93bef15c3266ddb1bc0455933
cecsun <ceciliesunde94@gmail.com>
2024-06-05 15:15:45 +0200

    Media queries

--- src/components/Nav/index.styles.js
+++ src/components/Nav/index.styles.js
@@ -2,9 +2,13 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Navbar = styled.nav`
-    background-color: red;
+    background-color: rebeccapurple;
     display: flex;
     justify-content: space-between;
+
+    @media (max-width: 500px) {
+        flex-direction: column;
+    }
 `;
 
 export const Logo = styled.div`
@@ -14,10 +18,20 @@ export const Logo = styled.div`
     align-items: center;
     margin-left: 20px;
     font-weight: bold;
+
+    @media (max-width: 700px) {
+        font-size: 20px;
+        justify-content: center;
+    }
 `;
 
 export const NavList = styled.ul`
     display: flex;
+
+    @media (max-width: 500px) {
+        display: flex;
+        justify-content: center;
+    }
 `;
 
 export const NavItem = styled.li`
@@ -29,9 +43,15 @@ export const NavItem = styled.li`
 export const StyledLink = styled(Link)`
     color: white;
     text-decoration: none;
+    text-transform: uppercase;
     font-size: 20px;
+    font-weight: 600;
     &:hover {
         color: black;
+    }
+
+    @media (max-width: 700px) {
+        font-size: 16px;
     }
 `;
 
